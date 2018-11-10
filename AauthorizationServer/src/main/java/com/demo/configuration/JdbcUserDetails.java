d3c0c76b62ea298534d1b1838a89d0c482e9d0d4
architect_pakistan@hotmail.com <architect_pakistan@hotmail.com>
2018-11-10 19:09:39 +0400

    removed unused files

--- AauthorizationServer/src/main/java/com/demo/configuration/JdbcUserDetails.java
+++ AauthorizationServer/src/main/java/com/demo/configuration/JdbcUserDetails.java
@@ -5,8 +5,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.demo.domain.Credentials;
 import com.demo.repository.CredentialRepository;
